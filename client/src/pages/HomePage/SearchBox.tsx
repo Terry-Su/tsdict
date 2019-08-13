66596fe84e3a664fc85b1893229f2dcffb5fbe74
TerrySu <2684759855@qq.com>
2019-08-13 22:49:12 +0800

    debounce search box

--- client/src/pages/HomePage/SearchBox.tsx
+++ client/src/pages/HomePage/SearchBox.tsx
@@ -1,3 +1,4 @@
+import debounce from 'lodash/debounce'
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
@@ -13,13 +14,28 @@ export default class SearchBox extends Component<Props> {
   searchingWordName?: string
   SET_SEARCHING_WORD_NAME?: Function
 
-  onChange = ( event ) => {
-    this.props.SET_SEARCHING_WORD_NAME( event.target.value )
+  state = {
+    valueProxy: this.searchingWordName,
   }
+
+  componentDidUpdate( prevProps ) {
+    if ( prevProps.searchingWordName !== this.searchingWordName && this.searchingWordName !== this.state.valueProxy ) {
+      this.setState( { valueProxy: this.searchingWordName } )
+    }
+  }
+
+  debouncedSetSearchingWordName = debounce( value => this.SET_SEARCHING_WORD_NAME( value ), 200 )
+
+  onChange =  ( event ) => {
+    this.setState( { valueProxy: event.target.value }, () => {
+      this.debouncedSetSearchingWordName( this.state.valueProxy )
+    } )
+  }
+
   render() {
     return (
       <StyledRoot>
-        <input value={ this.searchingWordName } onChange={ this.onChange }/>
+        <input value={ this.state.valueProxy } onChange={ this.onChange }/>
       </StyledRoot>
     )
   }
