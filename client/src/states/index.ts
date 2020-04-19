512e4f8ae53e5677ee8af67e16f396db5482e99e
Terry Su <theterrysu@163.com>
2020-04-19 22:48:57 +0800

    client-setting-origin: fix input's 'value' to 'defaultValue'

--- client/src/states/index.ts
+++ client/src/states/index.ts
@@ -3,6 +3,5 @@ import getDefaults from '../utils/getDefaults'
 const files = require.context('.', false, /\.ts$/)
 
 const ModelMap = getDefaults(files)
-console.log('ModelMap', ModelMap)
 
 export default ModelMap
